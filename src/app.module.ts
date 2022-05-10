b343c77a2927a3d6cb9934bd00a6f5b64683d05d
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-10 16:58:20 +0200

    Create the products controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
