66be8fc4a05873d796b52ec4bbf29a0be117f245
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-12 13:40:08 +0200

    add validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,8 @@
-export class CreateProductDto {}
+import { Decimal } from '@prisma/client/runtime';
+import { IsNotEmpty } from 'class-validator';
+export class CreateProductDto {
+    @IsNotEmpty()
+    name: string;
+    published : boolean;
+    price : Decimal;
+}
