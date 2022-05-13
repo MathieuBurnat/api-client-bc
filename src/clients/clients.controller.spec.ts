8f0487b7384b7ec17e1d72e23bad3c8ad0181e20
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-13 09:16:07 +0200

    Upgrade Unit Test for clients : Get many, one

--- src/clients/clients.controller.spec.ts
+++ src/clients/clients.controller.spec.ts
@@ -1,9 +1,21 @@
+import { Decimal } from '@prisma/client/runtime';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import prisma from '../../lib/prisma';
+import { CreateClientDto } from './dto/create-client.dto';
 
 describe('ClientsController', () => {
   let controller: ClientsController;
+  const clientGoal = {
+    id: expect.any(String),
+    card: expect.any(String),
+    firstname: expect.any(String),
+    lastname: expect.any(String),
+    createdAt: expect.any(Date),
+    updatedAt: expect.any(Date),
+    email: expect.any(String),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,5 +28,13 @@ describe('ClientsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  it('Get client - Find Many', async () => {
+    const result = await prisma.client.findMany();
+    expect(result[0]).toEqual(clientGoal);
+  });
+
+  it('Get client - Find one', async () => {
+    const result = await prisma.client.findFirst();
+    expect(result).toEqual(clientGoal);
   });
 });
