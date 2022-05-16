e5a4618933eec3afb7a37cae4e3dd6d34de98e32
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-05-16 14:46:24 +0200

    Upgrade tags on product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -3,7 +3,11 @@ import { isDate, IsNotEmpty, IsNumber, IsDate } from 'class-validator';
 export class CreateProductDto {
   @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   published: boolean;
+
+  @IsNotEmpty()
   qrcode: string;
 
   @IsNumber()
