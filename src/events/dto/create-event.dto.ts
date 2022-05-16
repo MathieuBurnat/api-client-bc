71adcbf5a57a413023e16a2e7e9be719fe11bc41
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-05-16 14:46:51 +0200

    Upgrade tags on event dto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -3,6 +3,10 @@ import { isDate, IsNotEmpty, IsNumber, IsDate } from 'class-validator';
 export class CreateEventDto {
     @IsNotEmpty()
     content: string;
+
+    @IsNotEmpty()
     productId: string;
+
+    @IsNotEmpty()
     eventTypeId: string;
 }
