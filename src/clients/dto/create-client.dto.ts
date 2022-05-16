6361a4f18a996657ebc4ec2953c736bb2b50e186
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-16 12:25:02 +0200

    pretty

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,12 +1,12 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateClientDto {
-    @IsEmail()
-    email: string;
-  
-    @IsNotEmpty()
-    firstname: string;
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    lastname: string;
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsNotEmpty()
+  lastname: string;
 }
