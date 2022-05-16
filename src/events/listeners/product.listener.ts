73cd30faba21ae8eda019460f6cb24acfff0db2c
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-05-16 17:28:55 +0200

    pretty

--- src/events/listeners/product.listener.ts
+++ src/events/listeners/product.listener.ts
@@ -3,18 +3,18 @@ import { OnEvent } from '@nestjs/event-emitter';
 
 @Injectable()
 export class ProductListener {
-    //Handle and process on created
-    @OnEvent('product.created')
-    handleCreatedEvent(product) {
-        console.log("A product has been created :)");
-        console.log(product);
-    }
+  //Handle and process on created
+  @OnEvent('product.created')
+  handleCreatedEvent(product) {
+    console.log('A product has been created :)');
+    console.log(product);
+  }
 
-    //Handle and process on retrieve
-    @OnEvent('product.retrieve')
-    handleRetrieveEvent(product) {
-        // handle and process "OrderCreatedEvent" event
-        console.log("A product has been retrieved by " + product.ownerId);
-        console.log(product);
-    }
+  //Handle and process on retrieve
+  @OnEvent('product.retrieve')
+  handleRetrieveEvent(product) {
+    // handle and process "OrderCreatedEvent" event
+    console.log('A product has been retrieved by ' + product.ownerId);
+    console.log(product);
+  }
 }
