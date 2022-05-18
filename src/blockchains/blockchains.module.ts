d44c674fe4a47dfe99f7f62a5db237d4e5b7bc34
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-18 18:21:14 +0200

    [Refactor] Create BigchaindbAdapter

--- src/blockchains/blockchains.module.ts
+++ src/blockchains/blockchains.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlockchainsService } from './blockchains.service';
 import { BlockchainsController } from './blockchains.controller';
+//import { BigchaindbAdapter } from './adapters/bigchain.adapter';
 
 @Module({
   controllers: [BlockchainsController],
-  providers: [BlockchainsService]
+  providers: [BlockchainsService],
 })
 export class BlockchainsModule {}
--- src/blockchains/blockchains.service.ts
+++ src/blockchains/blockchains.service.ts
@@ -1,46 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBlockchainDto } from './dto/create-blockchain.dto';
-import { UpdateBlockchainDto } from './dto/update-blockchain.dto';
-import { Ed25519Keypair, Transaction, Connection } from 'bigchaindb-driver';
+import { BigchaindbAdapter } from './adapters/bigchain.adapter';
 
 @Injectable()
 export class BlockchainsService {
+  private bigchaindbAdapter = new BigchaindbAdapter();
+
   async createTransaction(event, product) {
-    const tx = Transaction.makeCreateTransaction(
-      // Store the event, the product, and a timestamp
-      { event: event, product: product, created_at: new Date().toString() },
-
-      // Metadata contains information about the transaction itself
-      // (can be `null` if not needed)
-      { what: event.content },
-
-      // A transaction needs an output
-      [
-        Transaction.makeOutput(
-          Transaction.makeEd25519Condition(process.env.public_key),
-        ),
-      ],
-      process.env.public_key,
-    );
-
-    const txSigned = Transaction.signTransaction(tx, process.env.private_key);
-
-    const conn = new Connection(process.env.API_PATH);
-
-    return await conn
-      .postTransactionCommit(txSigned)
-      .then((retrievedTx) => retrievedTx);
+    return await this.bigchaindbAdapter.createTransaction(event, product);
   }
 
   async getTransactions(productId) {
-    const conn = new Connection(process.env.API_PATH);
-
-    return await conn.searchAssets(productId).then((assets) => assets);
+    return await this.bigchaindbAdapter.getTransactions(productId);
   }
 
-  // Dev options, use to generate an ed25519 keypair
-  // Normally keys should be buy on the internet (AKA wallet) but with bigchain service we can generate them with dev-tools
   async generateKeys() {
-    return await new Ed25519Keypair();
+    return await this.bigchaindbAdapter.generateKeys();
   }
 }
