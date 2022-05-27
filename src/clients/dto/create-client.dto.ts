b5ea3796767dfdb9858f59494b45a20c070711dc
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-27 18:38:16 +0200

    create the validator : isEmailUnique

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,7 +1,10 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmailUnique } from '../../validations/IsUnique';
+import { Validate } from 'class-validator';
 
 export class CreateClientDto {
   @IsEmail()
+  @Validate(IsEmailUnique)
   email: string;
 
   @IsNotEmpty()
