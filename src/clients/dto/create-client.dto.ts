d42740abb7145a7e46bc66b24c6269483992a22a
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-05-30 09:43:00 +0200

    rename the file "isUnique" to "clients-validator"

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { IsEmailUnique } from '../../validations/IsUnique';
+import { IsEmailUnique } from '../../validations/clients-validator';
 import { Validate } from 'class-validator';
 
 export class CreateClientDto {
