063442f994273931f798a0a64fce4b2e42f3f610
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-09 22:10:17 +0200

    [Refactor] transform functions to methods

--- src/blockchains/adapters/bigchain.adapter.ts
+++ src/blockchains/adapters/bigchain.adapter.ts
@@ -1,8 +1,6 @@
 import { Ed25519Keypair, Transaction, Connection } from 'bigchaindb-driver';
 import prisma from '../../../lib/prisma';
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { pluck } from 'rxjs';
-import e from 'express';
 export class BigchaindbAdapter {
   async createTransaction(event, product) {
     const tx = Transaction.makeCreateTransaction(
@@ -39,16 +37,18 @@ export class BigchaindbAdapter {
     return await conn.searchAssets(id).then((assets) => assets);
   }
 
-  // Verify eventy
-  async verifyEvents(productId) {
+  // certify eventy
+  async certifyEvents(productId) {
     // -- [ First check ] --
-    // Verify if the events from the database is the same than the events from the blockchain
+    // certify if the events from the database is the same than the events from the blockchain
 
     // *** Events on database ***
-    const DB_Events = await getPrettyDbEvents(productId);
+    const DB_Events = await this.getPrettyDbEvents(productId);
 
     // *** Events on blockchain ***
-    const BC_Events = await getPrettyBcEvents(await this.getAssets(productId));
+    const BC_Events = await this.getPrettyBcEvents(
+      await this.getAssets(productId),
+    );
 
     // ** Render the result **
     console.log('\n\nDB Events');
@@ -67,21 +67,21 @@ export class BigchaindbAdapter {
         const transaction = await this.getTransactions(asset[0].id);
 
         // -- [ Second check ] --
-        // Verify the authenticity of the events from the blockchain
+        // certify the authenticity of the events from the blockchain
         // Well the event's public key need to be known
 
         // Return the events with the tag 'verified' if it is
         // If the public key of the event if certified, set the tag certified to true
-        if (CertifyPublicKey(transaction.outputs[0].public_keys[0])) {
+        if (this.certifyPublicKey(transaction.outputs[0].public_keys[0])) {
           event = {
             ...BC_Events[i],
             certified: true,
           };
           console.log(
             '✅ Event ' +
-            event.id +
-            ' is certified by ' +
-            transaction.outputs[0].public_keys[0],
+              event.id +
+              ' is certified by ' +
+              transaction.outputs[0].public_keys[0],
           );
         } else {
           event = {
@@ -106,10 +106,6 @@ export class BigchaindbAdapter {
     return certifiedEvents;
   }
 
-  async verifyPublicKey(id) {
-    return 'Verified';
-  }
-
   // Generate an ed25519 keypair
   async generateKeys() {
     return await new Ed25519Keypair();
@@ -120,50 +116,50 @@ export class BigchaindbAdapter {
 
     return await conn.getTransaction(id).then((assets) => assets);
   }
-}
 
-// Certify the public key
-// Return true if the public key is certified
-async function CertifyPublicKey(publicKey) {
-  if (publicKey == process.env.public_key) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// Return the product's events within the database sorted by id
-async function getPrettyDbEvents(productId) {
-  // Get the events from the database
-  let DB_Events = await prisma.event.findMany({
-    where: {
-      productId,
-    },
-    include: {
-      eventType: true,
-    },
-  });
-
-  // Throw an error if there are no events in the database
-  if (DB_Events.length <= 0) {
-    throw new HttpException(
-      'We are sorry, this productId is referred to any event within the database',
-      HttpStatus.BAD_REQUEST,
-    );
+  // Certify the public key
+  // Return true if the public key is certified
+  async certifyPublicKey(publicKey) {
+    if (publicKey == process.env.public_key) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  // Sort Db events by id
-  DB_Events = DB_Events.sort((a, b) => a.id.localeCompare(b.id));
-  return DB_Events;
-}
+  // Return the product's events within the database sorted by id
+  async getPrettyDbEvents(productId) {
+    // Get the events from the database
+    let DB_Events = await prisma.event.findMany({
+      where: {
+        productId,
+      },
+      include: {
+        eventType: true,
+      },
+    });
 
-//Return the product's events on the blockchain sorted by id
-async function getPrettyBcEvents(BC_Events) {
-  // Pluck events
-  BC_Events = BC_Events.map((a) => a.data.event);
+    // Throw an error if there are no events in the database
+    if (DB_Events.length <= 0) {
+      throw new HttpException(
+        'We are sorry, this productId is referred to any event within the database',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
 
-  // Sort BC events by id
-  BC_Events = BC_Events.sort((a, b) => a.id.localeCompare(b.id));
+    // Sort Db events by id
+    DB_Events = DB_Events.sort((a, b) => a.id.localeCompare(b.id));
+    return DB_Events;
+  }
 
-  return BC_Events;
+  //Return the product's events on the blockchain sorted by id
+  async getPrettyBcEvents(BC_Events) {
+    // Pluck events
+    BC_Events = BC_Events.map((a) => a.data.event);
+
+    // Sort BC events by id
+    BC_Events = BC_Events.sort((a, b) => a.id.localeCompare(b.id));
+
+    return BC_Events;
+  }
 }
