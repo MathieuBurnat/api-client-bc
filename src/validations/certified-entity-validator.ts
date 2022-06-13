d09a76f896f3992926ba9d87c8f0f05cadb10aa4
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-13 23:47:22 +0200

    upgrade validator by faking a transaction

--- src/validations/certified-entity-validator.ts
+++ src/validations/certified-entity-validator.ts
@@ -2,6 +2,7 @@ import prisma from '../../lib/prisma';
 import { ValidatorConstraint, ValidationArguments } from 'class-validator';
 import { Injectable } from '@nestjs/common';
 import { ValidatorConstraintInterface } from 'class-validator';
+import { Ed25519Keypair, Transaction, Connection } from 'bigchaindb-driver';
 
 @ValidatorConstraint({ name: 'IsKeypairValid', async: true })
 @Injectable()
@@ -17,14 +18,32 @@ export class IsKeypairValid implements ValidatorConstraintInterface {
     });
 
     // If the entity doesnt exist, return false
-    if (ce) {
-      return true;
-    } else {
+    if (!ce) {
       return false;
     }
+    // Create and sign a transaction whitout publishing to the blockchain
+    // This is to test if the keypair is valid
+    let txSigned;
+    try {
+      const tx = Transaction.makeCreateTransaction(
+        { event: 'this is a test, it won\t be published to the bc' },
+        { what: 'validator test' },
+        [
+          Transaction.makeOutput(
+            Transaction.makeEd25519Condition(keypair.publicKey),
+          ),
+        ],
+        keypair.publicKey,
+      );
+      // Sign the transaction
+      txSigned = Transaction.signTransaction(tx, keypair.privateKey);
+    } catch (error) {
+      return false;
+    }
+    return true;
   }
 
   defaultMessage(args: ValidationArguments) {
-    return `The keypair is not valid. Is the public key known as a certified entity ?`;
+    return `The keypair is not valid. Is the public key known as a certified entity ? Are you sure than the keypair format is correct ?`;
   }
 }
