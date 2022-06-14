837bfa5d219bf3b8fbaf565402bec2eb6cb22f8e
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-06-15 00:43:19 +0200

    remove unused file

--- src/product-types/dto/update-product-type.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductTypeDto } from './create-product-type.dto';
-
-export class UpdateProductTypeDto extends PartialType(CreateProductTypeDto) {}
